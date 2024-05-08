7411a60587841e968a0d9ae631e0e0af1dc696b2
Mukesh Jangid <mukeshjangir29@gmail.com>
2024-05-08 14:52:22 +0530

    modularize nvim config

---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,14 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.showmode = false
+vim.g.have_nerd_font = true
+vim.opt.signcolumn = "yes"
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
