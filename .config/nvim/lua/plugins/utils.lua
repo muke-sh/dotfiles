62d525fe4d01cd4b21f659935efdc0937c3134ef
Mukesh Jangid <mukeshjangir29@gmail.com>
2024-05-11 01:02:22 +0530

    Add harpoon

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -9,7 +9,7 @@ return {
 				change = { text = "~" },
 				delete = { text = "_" },
 				topdelete = { text = "‾" },
-			changedelete = { text = "~" },
+				changedelete = { text = "~" },
 			},
 		},
 	},
