e464d4b0f90fdc4ca74dcd90762a2664241af0d7
Mukesh Jangid <mukeshjangir29@gmail.com>
2024-07-15 11:26:54 +0530

    Update gitsign

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -12,15 +12,6 @@ return {
 				changedelete = { text = "~" },
 				untracked = { text = "┆" },
 			},
-			signs_staged = {
-				add = { text = "┃" },
-				change = { text = "┃" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-				untracked = { text = "┆" },
-			},
-			signs_staged_enable = true,
 			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
 			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
